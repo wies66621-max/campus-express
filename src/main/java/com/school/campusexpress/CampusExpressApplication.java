12199fd1ec3b592ba15a3007f6de92a497b205e9
zxd <3130572247@qq.com>
2026-01-07 21:56:42 +0800

    环境配置

---
+++ src/main/java/com/school/campusexpress/CampusExpressApplication.java
@@ -0,0 +1,13 @@
+package com.school.campusexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/campusexpress/CampusExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.campusexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
