2fa6242729e8af385ff4f19940ee39e94aabb3bb
zxd <3130572247@qq.com>
2026-01-08 10:42:53 +0800

    环境配置

---
+++ src/main/java/com/school/campusexpress/common/R.java
@@ -0,0 +1,26 @@
+package com.school.campusexpress.common;
+
+import lombok.Data;
+
+@Data
+public class R<T> {
+
+    private int code;
+    private String message;
+    private T data;
+
+    public static <T> R<T> success(T data) {
+        R<T> r = new R<>();
+        r.setCode(200);
+        r.setMessage("成功");
+        r.setData(data);
+        return r;
+    }
+
+    public static <T> R<T> error(String message) {
+        R<T> r = new R<>();
+        r.setCode(500);
+        r.setMessage(message);
+        return r;
+    }
+}
--- src/main/java/com/school/campusexpress/controller/TestController.java
+++ src/main/java/com/school/campusexpress/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.school.campusexpress.controller;
 
+import com.school.campusexpress.common.R;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "后端启动成功";
+    public R<String> hello() {
+        return R.success("Hello Campus Express");
     }
 }
